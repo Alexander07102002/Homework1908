878d218d4cb0de095b33b07722ce37648bd5b715
Aleksandr <deadone.2018@inbox.ru>
2023-08-19 18:34:40 +0400

    Первое задание

---
+++ ZADACHA001_maxormin/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+int NumberA=int.Parse(Console.ReadLine());
+int NumberB=int.Parse(Console.ReadLine());
+
+int min =0;
+int max =0;
+
+if (NumberA>NumberB) {
+    max = NumberA;
+    min = NumberB;
+}
+else {
+    max=NumberB;
+    min=NumberA;
+}
+
+Console.WriteLine("Максимальное значение = "+max);
+Console.WriteLine("Минимальное значение = "+min);
